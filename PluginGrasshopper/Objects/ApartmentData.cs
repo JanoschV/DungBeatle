b055b5603ab5fa4c02ec62873488dfd94aab6e64
Janosch V <31912108+JanoschV@users.noreply.github.com>
2025-12-09 14:16:48 +0000

    ApartmentData being created

---
+++ PluginGrasshopper/Objects/ApartmentData.cs
@@ -0,0 +1,20 @@
+﻿using Rhino.Geometry;
+using Rhino.Input.Custom;
+using System.Collections.Generic;
+
+namespace DungBeatle.PluginGrasshopper.Objects
+{
+
+    public class ApartmentData
+    {
+
+        public string Pattern { get; set; }
+        public int NumberOfBedrooms  { get; set; }
+        public int NumberOfEnsuites { get; set; }
+        public int ApartmentArea { get; set; }
+        public int MinimumHallwayWidth { get; set; }
+
+        public ApartmentData(){ }
+    }
+
+}
